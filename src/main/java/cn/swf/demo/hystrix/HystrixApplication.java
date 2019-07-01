e1b56337368a121ef1b9738468e67a1544011548
swf <1101699259@qq.com>
2019-07-01 10:36:37 +0800

    删除无用pom

--- src/main/java/cn/swf/demo/hystrix/HystrixApplication.java
+++ src/main/java/cn/swf/demo/hystrix/HystrixApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 public class HystrixApplication {
 
 	public static void main(String[] args) {
